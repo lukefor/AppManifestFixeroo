3498af8abc489453f0e6c9742ebc43e17f9aef8c
Luke Foreman <admin@imgkk.com>
2014-06-18 17:16:10 +0100

    initial

---
+++ AppManifestFixeroo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace AppManifestFixeroo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] files = Directory.GetFiles(Directory.GetCurrentDirectory(), "appmanifest_*.acf");
+            if (files.Length == 0)
+                Console.WriteLine("No .acf files found in current dir.");
+            foreach (var file in files)
+            {
+                String rawKv = File.ReadAllText(file);
+                Match toDownload = Regex.Match(rawKv, @"""BytesToDownload""\s+""(\d+)""", RegexOptions.IgnoreCase);
+                Match downloaded = Regex.Match(rawKv, @"""BytesDownloaded""\s+""(\d+)""", RegexOptions.IgnoreCase);
+                if (toDownload.Groups[1].Value != downloaded.Groups[1].Value && Int32.Parse(downloaded.Groups[1].Value) > 0)
+                {
+                    Console.WriteLine(Path.GetFileName(file));
+                }
+            }
+        }
+    }
+}
